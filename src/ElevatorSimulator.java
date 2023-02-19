68142f76be64ea50872ec97f8177fd1fb58bb14d
Radek Szynwelski <121770151+radekszynwelski97@users.noreply.github.com>
2023-02-19 13:55:52 +0100

    Zabezpieczenie do windy

---
+++ src/ElevatorSimulator.java
@@ -0,0 +1,34 @@
+public class ElevatorSimulator {
+    public static void main(String[] args) {
+        Person person1 = new Person(1, 60);
+        Person person2 = new Person(2, 180);
+        Person person3 = new Person(3, 150);
+        Person person4 = new Person(4, 120);
+        Person person5 = new Person(5, 70);
+
+        //5 osób próbuje wsiąść
+        Elevator elevator = new Elevator();
+        elevator.add(person1);
+        elevator.add(person2);
+        elevator.add(person3);
+        elevator.add(person4);
+        elevator.add(person5);
+
+        //wszyscy wysiadają
+        elevator.clear();
+
+        //wsiadają 3 ciężkie osoby
+        elevator.add(person2);
+        elevator.add(person3);
+        elevator.add(person4);
+        elevator.start(); //winda przeciążona
+        //wszyscy wysiadają
+        elevator.clear();
+
+        //wsiadają dwie osoby, waga w normie
+        elevator.add(person1);
+        elevator.add(person2);
+        //winda może wystartować
+        elevator.start();
+    }
+}
